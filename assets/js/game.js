048584948967119fc28f402964f5b3bb25b40503
rprice000 <53877937+rprice000@users.noreply.github.com>
2021-08-31 18:19:54 -0500

    added fight multiple robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,7 +17,10 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+
+    // repeat and execute as long as the enemy-robot is alive
+while(enemyHealth > 0) {
+        //place fight fuction code block here
 
 // if player chooses to fight, then fight
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -81,8 +84,11 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     window.alert("You need to choose a valid option. Try again!");
 }
 
+}
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
